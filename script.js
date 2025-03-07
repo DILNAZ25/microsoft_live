eb83bb35fb745876fd1b11152dd61ebf73ca31f9
Dilnaz25 <dilnazzhaxylykova005@gmail.com>
2025-03-07 19:14:35 +0500

    new settings

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const showsign = document.getElementById("show-sign");
+document.addEventListener("DOMContentLoaded", () => {
+    const signInOptions = document.getElementById("show-sign");
     const back = document.getElementById("back");
     const wrapper = document.getElementById("wrapper");
     const options = document.getElementById("options");
+    const overlay = document.getElementById("overlay");
+    overlay.classList.add("hidden");
 
-    showsign.addEventListener("click", () => {
+    signInOptions.addEventListener("click", () => {
         wrapper.classList.add("hidden");
-        options.classList.remove("show");
+        signInOptions.classList.add("hidden");
+        options.classList.remove("hidden");
+        options.classList.add("show");
+        overlay.classList.remove("hidden");
+        overlay,classList.add("show");
     });
 
     back.addEventListener("click", () => {
         options.classList.add("hidden");
-        wrapper.classList.remove("show");
+        options.classList.remove("show");
+        wrapper.classList.remove("hidden");
+        signInOptions.classList.remove("hidden");
+        overlay.classList.add("hidden");
+        overlay.classList.remove("show");
+
     });
+
+    document.getElementById("show-sign").addEventListener("click", () => {
+        document.getElementById("options").classList.add("show");
+    
+    });
+    
+    document.getElementById("back").addEventListener("click", () => {
+        document.getElementById("options").classList.remove("show");
+    
+    });    
+});
